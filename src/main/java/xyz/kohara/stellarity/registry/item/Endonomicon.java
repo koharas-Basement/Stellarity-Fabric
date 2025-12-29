9c1960d206eea8d2422d4f12bdbacb815e3e8913
Coder2195 <coder2195mail@gmail.com>
2025-12-29 17:10:53 -0500

    ✨ patcholoui FIXED

--- src/main/java/xyz/kohara/stellarity/registry/item/Endonomicon.java
+++ src/main/java/xyz/kohara/stellarity/registry/item/Endonomicon.java
@@ -3,6 +3,7 @@ package xyz.kohara.stellarity.registry.item;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.Level;
 //? < 1.21.9 {
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.ItemStack;
+import xyz.kohara.stellarity.Stellarity;
 //? } else {
 /*import net.minecraft.world.InteractionResult;
  *///? }
@@ -27,16 +29,36 @@ public class Endonomicon extends Item {
 
     @Override
     public /*? < 1.21.9 { */InteractionResultHolder<ItemStack>/*? } else { */ /*InteractionResult*//*? }*/ use(Level level, Player player, InteractionHand interactionHand) {
-        boolean patchouliLoaded = FabricLoader.getInstance().isModLoaded("patchouli");
 
-        if (!patchouliLoaded && player instanceof LocalPlayer localPlayer) {
+
+        var result = super.use(level, player, interactionHand);
+
+        if (!(player instanceof LocalPlayer localPlayer)) return result;
+
+
+        boolean patchouliLoaded = FabricLoader.getInstance().isModLoaded("patchouli");
+        if (!patchouliLoaded) {
             localPlayer.displayClientMessage(
                 Component.translatable("message.stellarity.missing_patchouli"), true
+            );
+
+            return result;
+        }
+
+        try {
+            Object apiInstance = Class.forName("vazkii.patchouli.api.PatchouliAPI").getDeclaredMethod("get").invoke(Object.class);
+
+            apiInstance.getClass().getMethod("openBookGUI", ResourceLocation.class).invoke(apiInstance, Stellarity.id("endonomicon"));
+
+
+        } catch (ReflectiveOperationException e) {
+            localPlayer.displayClientMessage(
+                Component.literal(e.toString()), false
             );
         }
 
 
-        return super.use(level, player, interactionHand);
+        return result;
 
 
     }
